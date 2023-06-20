71ae0c88893a1e7f44ee44c4a8ea7cb9633f7120
TJ <tj@itst.co.kr>
2023-06-20 10:53:57 +0900

    exe1 kickstart gui exe2

--- exe1/main.cpp
+++ exe1/main.cpp
@@ -7,5 +7,6 @@ using namespace std;
 
 int main() {
     cout << "exe1 START" << endl;
+    cout << system("launchctl kickstart gui/501/kr.itst.exe2.plist") << endl;
     return 0;
 }
--- exe2/main.cpp
+++ exe2/main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 int main() {
     cout << "exe2 START" << endl;
+    cout << system("echo $(id -u)") << endl;
+    cout << system("open -W '/Users/tj/Downloads/setup.pkg'") << endl;
     return 0;
 }
