469b550b3f1c26cb748d7411859af6db8a5ffb14
TJ <tj@itst.co.kr>
2023-06-20 15:51:07 +0900

    socket

--- exe1/main.cpp
+++ exe1/main.cpp
@@ -2,11 +2,34 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 using namespace std;
 
 int main() {
     cout << "exe1 START" << endl;
-    cout << system("launchctl kickstart gui/501/kr.itst.exe2.plist") << endl;
+    auto socket_path = "/tmp/my_socket.sock";
+    unlink(socket_path);
+    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1)
+        return 1;
+    sockaddr_un addr;
+    memset(&addr, 0, sizeof(sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+    if (::bind(sockfd, (sockaddr*)&addr, sizeof(sockaddr_un)) == -1)
+        return 2;
+    if (listen(sockfd, SOMAXCONN) == -1)
+        return 3;
+    int clientSock = accept(sockfd, nullptr, nullptr);
+    if (clientSock == -1)
+        return 4;
+    char buffer[1024];
+    auto bytesRead = read(clientSock, buffer, sizeof(buffer));
+    if (bytesRead == -1)
+        return 5;
+    cout << "Received msg: " << buffer << endl;
+    close(sockfd);
     return 0;
 }
--- exe2/main.cpp
+++ exe2/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main() {
     cout << "exe2 START" << endl;
-    cout << system("echo $(id -u)") << endl;
-    cout << system("open -W '/Users/tj/Downloads/setup.pkg'") << endl;
+    auto socket_path = "/tmp/my_socket.sock";
+    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1)
+        return 1;
+    sockaddr_un addr;
+    memset(&addr, 0, sizeof(sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+    if (connect(sockfd, (sockaddr*)&addr, sizeof(sockaddr_un)) == -1)
+        return 2;
+    auto msg = "Hello, Server!";
+    auto bytesSent = send(sockfd, msg, strlen(msg), 0);
+    if (bytesSent == -1)
+        return 3;
+    close(sockfd);
     return 0;
 }
